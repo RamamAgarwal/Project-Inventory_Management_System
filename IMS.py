4c359178f1fbce43b406e58588e011778f74de6e
RamamAgarwal <ramamagarwal1234@gmail.com>
2024-07-18 12:59:54 +0530

    update

--- IMS.py
+++ IMS.py
@@ -10,21 +10,33 @@ updated_prod_lst= []
 
 for product in products:
     prod_details=product.split(',')
-    
+
     if prod_details[0]==ui_prod_id:
-        print("----------------------------------")
-        print("Product Name   :        ",prod_details[1])
-        print("Price          :        ",prod_details[2])
-        print("Quantity       :        ",ui_prod_quan)
-        print("----------------------------------")
-        print("Billing Amount :        ",int(prod_details[2])*int(ui_prod_quan))
-        print("----------------------------------")
+
+        if int(ui_prod_quan) <= int(prod_details[3]):
+
+            print("----------------------------------")
+            print("Product Name   :        ",prod_details[1])
+            print("Price          :        ",prod_details[2])
+            print("Quantity       :        ",ui_prod_quan)
+            print("----------------------------------")
+            print("Billing Amount :        ",int(prod_details[2]) * int(ui_prod_quan))
+            print("----------------------------------")
+
+            prod_details[3]= str(int(prod_details[3]) - int(ui_prod_quan))
+        updated_prod_lst.append(prod_details)
     
-    prod_details[3]= int(prod_details[3]) - int(ui_prod_quan)
-    updated_prod_lst.append(prod_details)
-    
-fm=open("Inventory.txt",'w')
+lst=[]
+
 for i in updated_prod_lst:
-    prod = i[0] + "," + i[1] + "," + i[2] + "," + str(i[3]) + "\n"
-    fm.write(prod)
+    prod = i[0] + "," + i[1] + "," + i[2] + "," + i[3] + "\n"
+    lst.append(prod)
+
+lst[-1]=lst[-1][:-1]
+
+fm=open("Inventory.txt",'w')
+
+for i in lst:
+    fm.write(i)
+
 fm.close()
